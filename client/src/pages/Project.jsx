e08c71780df7eb36bc905b0d63cbe1c763e91b29
Purity Gwaro <puritygwaro@gmail.com>
2022-06-08 18:37:30 +0300

    added project page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
+import ClientInfo from "../components/ClientInfo";
 
 export default function Project() {
     //get the id
@@ -15,6 +16,17 @@ export default function Project() {
     if (error) return <p>Something Went Wrong</p>;
 
   return (
-    <div>Project</div>
+    <>
+        { !loading && !error && (
+            <div className="mx-auto w-75 card p-5">
+                <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">Back</Link>
+                <h1 className="text-center">{data.project.name}</h1>
+                <p>{data.project.description}</p>
+                <h5 className="mt-3">Project Status: </h5>
+                <p className="lead">{ data.project.status }</p>
+                <ClientInfo client={data.project.client}/>
+            </div>
+        )}
+    </>
   )
 }
