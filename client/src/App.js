39834a557f8d73e2cbd5ec98185e5dc913275b7c
Purity Gwaro <puritygwaro@gmail.com>
2022-06-08 10:49:37 +0300

    adding client side

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+
+//<> is the beginning fragment </> is the end fragment
+function App() {
+  return (
+     <>   
+      <Header />
+      <div className="Container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
