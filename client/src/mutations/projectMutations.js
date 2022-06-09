02328e4b268949a731a41cf2e4e28d49192e89f4
Purity Gwaro <puritygwaro@gmail.com>
2022-06-09 14:54:28 +0300

    added update/edit mutation for project details

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -2,8 +2,17 @@ import { gql } from "@apollo/client";
 
 
 const ADD_PROJECT = gql`
-    mutation AddProject($name: String!, $description: String!,$status: ProjectStatus!, $clientId: ID!) {
-        addProject(name: $name, description: $description, status: $status, clientId: $clientId) {
+    mutation AddProject(
+        $name: String!, 
+        $description: String!,
+        $status: ProjectStatus!, 
+        $clientId: ID!) {
+        addProject(
+            name: $name, 
+            description: $description, 
+            status: $status, 
+            clientId: $clientId
+            ) {
             id
             name
             status
@@ -26,8 +35,35 @@ const DELETE_PROJECT = gql`
 
 `;
 
+const UPDATE_PROJECT = gql`
+    mutation UpdateProject(
+        $id: ID!,
+        $name: String!, 
+        $description: String!,
+        $status: ProjectStatusUpdate!, 
+        ) {
+        updateProject(
+            id: $id,
+            name: $name, 
+            description: $description, 
+            status: $status, 
+            ) {
+            id
+            name
+            status
+            client {
+                id
+                name
+                email
+                phone
+            }
+    }
+}
+`;
+
 export { 
     ADD_PROJECT,
     DELETE_PROJECT,
+    UPDATE_PROJECT
 };
  
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -6,6 +6,7 @@ import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 import ClientInfo from "../components/ClientInfo";
 import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 
 export default function Project() {
     //get the id
@@ -27,6 +28,7 @@ export default function Project() {
                 <p className="lead">{ data.project.status }</p>
                 <ClientInfo client={data.project.client}/>
 
+                <EditProjectForm project={data.project} />
                 <DeleteProjectButton projectId={data.project.id}/> 
             </div>
         )}
